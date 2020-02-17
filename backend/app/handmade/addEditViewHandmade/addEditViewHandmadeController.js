5f1745078aa34982b80a0f824d3bf5135f46e358
Karun Kalantabutra <pondkarun@gmail.com>
2020-02-17 13:23:05 +0700

    login

--- backend/app/handmade/addEditViewHandmade/addEditViewHandmadeController.js
+++ backend/app/handmade/addEditViewHandmade/addEditViewHandmadeController.js
@@ -1,7 +1,7 @@
 'use strict'
 
 app.controller("addEditViewHandmadeController", ['$scope', '$rootScope', '$location', '$routeParams', 'userService', '$http', 'customDialog', 'msgSettings', 'commonService',
-    function($scope, $rootScope, $location, $routeParams, userService, $http, customDialog, msgSettings, commonService) {
+    function ($scope, $rootScope, $location, $routeParams, userService, $http, customDialog, msgSettings, commonService) {
         let _this = this;
         this.modelSave = {
             name: null,
@@ -19,7 +19,10 @@ app.controller("addEditViewHandmadeController", ['$scope', '$rootScope', '$locat
             }
             _this.modelSave.size.push(item);
         }
+        this.removable = (item) => {
+            console.log("item", item);
 
+        }
         this.mapchipModel = (item) => {
             _this.model.size = [];
             _this.modelSave.size.filter((e) => {
@@ -31,7 +34,7 @@ app.controller("addEditViewHandmadeController", ['$scope', '$rootScope', '$locat
 
         this.typePage = {};
         $scope.isView = false;
-        this.init = function() {
+        this.init = function () {
             checkRouteParams()
             getType()
         }
@@ -66,7 +69,7 @@ app.controller("addEditViewHandmadeController", ['$scope', '$rootScope', '$locat
 
         function uploadImage(id) {
             var fd = new FormData();
-            angular.forEach($scope.files, function(file) {
+            angular.forEach($scope.files, function (file) {
                 fd.append('file[]', file);
             });
             fd.append('formdata', JSON.stringify(id));
